f4c3ab1cc235ba612dd68b00cfd80cd275d7a2dc
Bela Géréon <belagereon@gmx.de>
2026-01-26 17:46:44 +0100

    :poop: integrating example dashboard and its dependencies, still broken though

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import ProTip from "./ProTip";
+import AppTheme from "./shared-theme/AppTheme";
+import Dashboard from "./dashboard/Dashboard";
 
 function Copyright() {
   return (
@@ -11,13 +13,13 @@ function Copyright() {
       variant="body2"
       align="center"
       sx={{
-        color: 'text.secondary',
+        color: "text.secondary",
       }}
     >
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}.
     </Typography>
   );
@@ -25,14 +27,8 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <AppTheme>
+      <Dashboard />
+    </AppTheme>
   );
 }
